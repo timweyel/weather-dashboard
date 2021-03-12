61c79359e62d184bb84804bc1268719bba3c14da
Tim Weyel <tweyel@gmail.com>
2021-03-11 16:37:53 -0800

    added html structure. started on js to get currentWeather...not working yet :(

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,54 @@
-let api_key = '437055076a04e82223227a4c0e154c80';
+let citySearchHistory = [];
 
-let queryURL = 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}';
+let api_key = '437055076a04e82223227a4c0e154c80';
+let queryURL = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+
+let cityInputEL = document.querySelector("#city-input");
+let currentWeatherEL = document.querySelector("#currentWeather");
+let forecastWeatherEL = document.querySelector("#forecastWeather");
+
+
+//fetch current weather for searched city
+let getCityWeather = function(city) {
+  
+  fetch(queryURL)
+  .then(function(response) {
+    response.json()
+    .then(function(data) {
+      displayCurrentWeather(data, city);
+    });
+  });
+};
+
+let displayCurrentWeather = function(weather, searchedCity) {
+
+  //clear out any previous search result
+  currentWeather.textContent = '';
+  
+  //date
+  let currentDate = document.createElement("span");
+  currentWeather.textContent = " (" + moment(weather.dt.value).format("MMM D, YYYY") + ") "; 
+  cityInput.appendChild(currentDate);
+
+  //temperature
+  let temperatureEl = document.createElement("span");
+  temperatureEl.textContent = "Temperature: " + weather.main.temp + " F";
+  currentWeather.appendChild(temperatureEl);
+
+  //humidity
+  let humidityEl = document.createElement("span");
+  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+  currentWeather.displayCurrentWeather(humidityEl);
+
+  //wind speed
+  let windSpeedEl = document.createElement("span");
+  windSpeedEl.textContent = "Wind Speed: " + weather.main.speed + " MPH";
+  currentWeather.displayCurrentWeather(windSpeedEl);
+
+  //TODO: need to figure out uv index
+  //uv index
+}
+
+let displayFiveDayForecast = function() {
+  
+}
