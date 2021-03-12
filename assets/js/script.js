9e666b68bdfee35757472a8584fe8404d63b3dc9
Tim Weyel <tweyel@gmail.com>
2021-03-12 12:59:25 -0800

    refactored some html ids. started over on js. fetch is working! yahoo!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,30 @@
 let citySearchHistory = [];
 
-let api_key = '437055076a04e82223227a4c0e154c80';
-let queryURL = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+let searchColEl = document.querySelector("#search-col");
+let cityInputEl = document.querySelector("#city-search-input");
+let searchHistoryEl = document.querySelector("#search-history");
+let currentWeatherEl = document.querySelector("#currentWeather");
+let fiveDayForecastContainerEl = document.querySelector("#fiveDayForecastContainer");
+let fiveDayForecastCardsContainerEl = document.querySelector("#fiveDayForecastCardsContainer");
 
-let cityInputEL = document.querySelector("#city-input");
-let currentWeatherEL = document.querySelector("#currentWeather");
-let forecastWeatherEL = document.querySelector("#forecastWeather");
+let api_key = '437055076a04e82223227a4c0e154c80';
+let city = 'san francisco';
+let queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=437055076a04e82223227a4c0e154c80`;
+
 
 
 //fetch current weather for searched city
 let getCityWeather = function(city) {
   
-  fetch(queryURL)
+  fetch(queryUrl)
   .then(function(response) {
-    response.json()
+    return response.json()
     .then(function(data) {
-      displayCurrentWeather(data, city);
+      console.log(data)
+      //displayCurrentWeather(data, city);
     });
   });
 };
 
-let displayCurrentWeather = function(weather, searchedCity) {
 
-  //clear out any previous search result
-  currentWeather.textContent = '';
-  
-  //date
-  let currentDate = document.createElement("span");
-  currentWeather.textContent = " (" + moment(weather.dt.value).format("MMM D, YYYY") + ") "; 
-  cityInput.appendChild(currentDate);
 
-  //temperature
-  let temperatureEl = document.createElement("span");
-  temperatureEl.textContent = "Temperature: " + weather.main.temp + " F";
-  currentWeather.appendChild(temperatureEl);
-
-  //humidity
-  let humidityEl = document.createElement("span");
-  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
-  currentWeather.displayCurrentWeather(humidityEl);
-
-  //wind speed
-  let windSpeedEl = document.createElement("span");
-  windSpeedEl.textContent = "Wind Speed: " + weather.main.speed + " MPH";
-  currentWeather.displayCurrentWeather(windSpeedEl);
-
-  //TODO: need to figure out uv index
-  //uv index
-}
-
-let displayFiveDayForecast = function() {
-  
-}
