8a7d58529f4386cb985ac3f198a4d6875b7b5cd8
Tim Weyel <tweyel@gmail.com>
2021-03-14 17:25:27 -0700

    added all the current weather elements except UV index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
+// $(document).ready(function(event) {
+
 let citySearchHistory = [];
 
-let searchCityFormEl = document.querySelector("#citySearchForm");
+let searchInputEl = document.querySelector("#city-search-input");
+
+let currentWeatherContainerEl = document.querySelector('#current-weather-container');
+
+let searchInputFormEl = document.querySelector("#citySearchForm");
 let searchColEl = document.querySelector("#search-col");
 
 let searchButton = document.querySelector("#search-button");
-let cityInputEl = document.querySelector("#city-search-input");
 let searchHistoryEl = document.querySelector("#search-history");
 
 let fiveDayForecastContainerEl = document.querySelector("#fiveDayForecastContainer");
@@ -19,31 +24,56 @@ let getCityWeather = function() {
   const city = document.querySelector("#city-search-input").value;
   //console.log('city before fetch', city);
   
-  let queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;  
+  let queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${api_key}`;  
   
   fetch(queryUrl)
   .then(function(response) {
     return response.json()
     .then(function(data) {
-      console.log(data)
-
-      //console.log('city after fetch', city);
-      let searchCity = document.querySelector("#searchCity");
-      searchCity.append(city);
-
-      let todaysDateEl = document.querySelector("#todaysDate");
-      todaysDateEl.textContent=moment(data.dt.value).format("MMM D, YYYY");
-      currentWeatherContainerEl.append(todaysDateEl);
-
-      var weatherIcon = document.createElement("img")
-      weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
-      currentWeatherContainerEl.append(weatherIcon);
-
-
-
+      console.log(data);
+      displayWeather(data, city);
+      
     });
-  });
 
+  });
+  //console.log(city);
 };
 
+let displayWeather = function(weather, city) {
+// console.log(city);
+  currentWeatherContainerEl.textContent = '';
+  currentWeatherContainerEl.append(city);
 
+  // let searchInput = document.querySelector("#searchInput");
+  // searchInput.appendChild(city);
+
+  var currentDate = document.createElement("span")
+  currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+  currentWeatherContainerEl.appendChild(currentDate);
+
+  //create an image element
+  var weatherIcon = document.createElement("img")
+  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+  currentWeatherContainerEl.appendChild(weatherIcon);
+
+  //create a span element to hold temperature data
+  var temperatureEl = document.createElement("span");
+  temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+  currentWeatherContainerEl.appendChild(temperatureEl);
+  temperatureEl.classList = "list-group-item";
+  
+  //create a span element to hold Humidity data
+  var humidityEl = document.createElement("span");
+  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+  currentWeatherContainerEl.appendChild(humidityEl);
+  humidityEl.classList = "list-group-item";
+
+  //create a span element to hold Wind data
+  var windSpeedEl = document.createElement("span");
+  windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+  currentWeatherContainerEl.appendChild(windSpeedEl);
+  windSpeedEl.classList = "list-group-item";
+    
+}
+
+// });
