013e5e6d4455aff59ad8af870244592271f1c093
Tim Weyel <tweyel@gmail.com>
2021-03-17 11:32:27 -0700

    finally got card-group to stay under heading

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const citySearchHistory = [];
 // let searchInputEl = document.querySelector("#city-search-input");
 
 let currentWeatherContainerEl = document.querySelector('#current-weather-container');
+let currentWeatherDetailsEl = document.querySelector('#current-weather-details');
 
 let searchInputFormEl = document.querySelector("#citySearchForm");
 let searchColEl = document.querySelector("#search-col");
@@ -10,8 +11,11 @@ let searchColEl = document.querySelector("#search-col");
 let searchButton = document.querySelector("#search-button");
 let searchHistoryEl = document.querySelector("#search-history");
 
-let fiveDayForecastContainerEl = document.querySelector("#fiveDayForecastContainer");
-let fiveDayForecastCardsContainerEl = document.querySelector("#fiveDayForecastCardsContainer");
+let fiveDayForecastHeader = document.querySelector("#five-day-forecast-cards");
+
+
+//let fiveDayForecastCardsContainerEl = document.querySelector("#fiveDayForecastCardsContainer");
+// fiveDayForecastContainerEl.appendChild(fiveDayForecastCardsContainerEl);
 
 
 const api_key = '437055076a04e82223227a4c0e154c80';
@@ -37,12 +41,13 @@ const getCityWeather = function() {
 const displayCurrentWeather = function(weather, city) {
 // console.log(city);
   currentWeatherContainerEl.textContent = '';
-  currentWeatherContainerEl.append(city);
+  let searchedCityEl = document.createElement("h2");
+  searchedCityEl.textContent = `${city}`;
+  searchedCityEl.setAttribute("vertical-align", "center");
+  currentWeatherContainerEl.append(searchedCityEl);
 
-  // let searchInput = document.querySelector("#searchInput");
-  // searchInput.appendChild(city);
 
-  let currentDate = document.createElement("span")
+  let currentDate = document.createElement("h2")
   currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
   currentWeatherContainerEl.appendChild(currentDate);
 
@@ -54,20 +59,24 @@ const displayCurrentWeather = function(weather, city) {
   //create a span element to hold temperature data
   let temperatureEl = document.createElement("span");
   temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
-  currentWeatherContainerEl.appendChild(temperatureEl);
+  currentWeatherDetailsEl.appendChild(temperatureEl);
   temperatureEl.classList = "list-group-item";
   
   //create a span element to hold Humidity data
   let humidityEl = document.createElement("span");
   humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
-  currentWeatherContainerEl.appendChild(humidityEl);
+  currentWeatherDetailsEl.appendChild(humidityEl);
   humidityEl.classList = "list-group-item";
 
   //create a span element to hold Wind data
   let windSpeedEl = document.createElement("span");
   windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
-  currentWeatherContainerEl.appendChild(windSpeedEl);
+  currentWeatherDetailsEl.appendChild(windSpeedEl);
   windSpeedEl.classList = "list-group-item";
+
+  let uvIndexEl = document.createElement("span");
+  humidityEl.textContent = "UV Index goes here";
+  currentWeatherDetailsEl.appendChild(uvIndexEl);
     
   getFiveDayForecast(city);
 }
@@ -86,41 +95,56 @@ const getFiveDayForecast = function(city){
 };
 
 const displayFiveDayForecast = function(weather) {
-
-  //fiveDayForecastContainerEl.textContent = '';
-  let fiveDayForecastHeader = document.createElement("h1");
-  fiveDayForecastHeader.textContent = "5-Day Forecast";
-  fiveDayForecastContainerEl.appendChild(fiveDayForecastHeader);
+  let fiveDayForecastContainerEl = document.querySelector("#five-day-forecast-container");
+    fiveDayForecastContainerEl.textContent = '';
   
+  // let fiveDayForecastHeaderEl = document.createElement("div");
+  // fiveDayForecastContainerEl.appendChild(fiveDayForecastHeaderEl);
+  
+  
+    fiveDayForecastHeader.classList = "col-12 h2";
+    fiveDayForecastHeader.textContent = "5-Day Forecast";
+    fiveDayForecastContainerEl.appendChild(fiveDayForecastHeader);
+
+  let fiveDayForecastCardGroup = document.createElement("div");
+    fiveDayForecastCardGroup.classList = "card-group";
+    fiveDayForecastContainerEl.appendChild(fiveDayForecastCardGroup);
+
   let fiveDayForecast = weather.list;
     for (let i=5; i<fiveDayForecast.length; i+=8) {
       let daily = fiveDayForecast[i];
       console.log(daily); 
+
+      let fiveDayForecastCards = document.querySelector("#five-day-forecast-cards");
+      fiveDayForecastCards = document.createElement("div");
+      fiveDayForecastCards.classList = "card";
+      fiveDayForecastCardGroup.appendChild(fiveDayForecastCards);      
+      
+      let fiveDayForecastDate = document.createElement("h4");
+      fiveDayForecastDate.classList = "card-header";
+      fiveDayForecastDate.textContent = moment.unix(daily.dt).format("MMM D, YYYY");
+
+      fiveDayForecastCards.appendChild(fiveDayForecastDate);
+
+      let fiveDayForecastIcon = document.createElement("img");
+      fiveDayForecastIcon.setAttribute("src", `https://openweathermap.org/img/wn/${daily.weather[0].icon}@2x.png`);  
+      fiveDayForecastIcon.classList = "card-body text-center ";
+      fiveDayForecastIcon.setAttribute("width", '100px');  
+      fiveDayForecastCards.appendChild(fiveDayForecastIcon);
     
+      let fiveDayForecastTemp = document.createElement("span");
+      fiveDayForecastTemp.classList = "card-body text-center";
+      fiveDayForecastTemp.textContent = daily.main.temp + " °F";
+      fiveDayForecastCards.appendChild(fiveDayForecastTemp);
+    
+      let fiveDayForecastHumidity=document.createElement("span");
+      fiveDayForecastHumidity.classList = "card-body text-center";
+      fiveDayForecastHumidity.textContent = daily.main.humidity + "  %";
+      fiveDayForecastCards.appendChild(fiveDayForecastHumidity);
 
-  let fiveDayForecastCards = document.createElement("div");
-  fiveDayForecastCards.classList = "card";
-  fiveDayForecastContainerEl.appendChild(fiveDayForecastCards);
-
-  let fiveDayForecastDate = document.createElement("h4");
-  fiveDayForecastDate.textContent = moment.unix(daily.dt).format("MMM D, YYYY");
-  fiveDayForecastDate.classList = "card-header";
-  fiveDayForecastCards.appendChild(fiveDayForecastDate);
-
-  let fiveDayForecastIcon = document.createElement("img");
-  fiveDayForecastIcon.setAttribute("src", `https://openweathermap.org/img/wn/${daily.weather[0].icon}@2x.png`);  
-  fiveDayForecastIcon.classList = "card-body text-center mx-auto";
-  fiveDayForecastIcon.setAttribute("width", '100px');  
-  fiveDayForecastCards.appendChild(fiveDayForecastIcon);
-
-  let fiveDayForecastTemp = document.createElement("span");
-  fiveDayForecastTemp.classList = "card-body text-center";
-  fiveDayForecastTemp.textContent = daily.main.temp + " °F";
-  fiveDayForecastCards.appendChild(fiveDayForecastTemp);
-
-  let fiveDayForecastHumidity=document.createElement("span");
-  fiveDayForecastHumidity.classList = "card-body text-center";
-  fiveDayForecastHumidity.textContent = daily.main.humidity + "  %";
-  fiveDayForecastCards.appendChild(fiveDayForecastHumidity);
-}
+      let fiveDayForecastUvIndex = document.createElement("span");
+      fiveDayForecastUvIndex.classList = "card-body text-center";
+      fiveDayForecastUvIndex.textContent = 'UV Index here';
+      fiveDayForecastCards.appendChild(fiveDayForecastUvIndex);
+    }
 };
