4df7dcc87679f3a1c7b79b8a1ddc01037e468431
Tim Weyel <tweyel@gmail.com>
2021-03-18 13:50:29 -0700

    added UV Index

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,12 @@ let searchInputFormEl = document.querySelector("#citySearchForm");
 let searchColEl = document.querySelector("#search-col");
 
 let searchButton = document.querySelector("#search-button");
-let searchHistoryEl = document.querySelector("#search-history");
+
 
 let fiveDayForecastHeader = document.querySelector("#five-day-forecast-cards");
 
-
-//let fiveDayForecastCardsContainerEl = document.querySelector("#fiveDayForecastCardsContainer");
-// fiveDayForecastContainerEl.appendChild(fiveDayForecastCardsContainerEl);
-
-
 const api_key = '437055076a04e82223227a4c0e154c80';
+
 
 //fetch current weather for searched city
 const getCityWeather = function() {
@@ -32,11 +28,15 @@ const getCityWeather = function() {
   .then(function(response) {
     response.json()
     .then(function(data) {
-      console.log(data);
+      //console.log(data);
       displayCurrentWeather(data, city);
     });
   });
-  //console.log(city);
+
+};
+
+const saveSearch = function() {
+  localStorage.setItem("citySearchHistory", JSON.stringify(citySearchHistory));
 };
 
 const displayCurrentWeather = function(weather, city) {
@@ -75,21 +75,59 @@ const displayCurrentWeather = function(weather, city) {
   currentWeatherDetailsEl.appendChild(windSpeedEl);
   windSpeedEl.classList = "list-group-item";
 
-  let uvIndexEl = document.createElement("span");
-  humidityEl.textContent = "UV Index goes here";
-  currentWeatherDetailsEl.appendChild(uvIndexEl);
+  let latitude = weather.coord.lat;
+  let longitude = weather.coord.lon;
+
+  getUvIndex(latitude,longitude)
     
   getFiveDayForecast(city);
 }
 
-const getFiveDayForecast = function(city){
+const getUvIndex = function(latitude, longitude) {
+  const queryUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&appid=${api_key}`;
+
+  fetch(queryUrl)
+  .then(function(response){
+      response.json()
+      .then(function(data){
+         displayUvIndex(data)
+         console.log(data);
+         console.log(latitude);
+         console.log(longitude);
+         console.log(data.value);
+      });
+  });
+}
+
+const displayUvIndex = function(index) {
+  let uvIndexEl = document.createElement("span");
+  uvIndexEl.textContent = "UV Index: ";
+  uvIndexEl.classList = "list-group-item";
+  currentWeatherDetailsEl.appendChild(uvIndexEl);
+
+  uvIndexShading = document.createElement("span");
+  uvIndexShading.textContent = index.value;
+
+  if (index.value <= 2.9) {
+    uvIndexShading.classList = "low";    
+  } else if (index.value >= 3 && index.value <= 7.9) {
+    uvIndexShading.classList = "medium";
+  } else if (index.value >= 8) {
+    uvIndexShading.classList = "high";
+  }
+
+  uvIndexEl.appendChild(uvIndexShading);
+
+}
+
+const getFiveDayForecast = function(city){  
   const queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${api_key}`;
 
   fetch(queryUrl)
   .then(function(response){
       response.json()
       .then(function(data){
-        console.log(data)
+        //console.log(data)
       displayFiveDayForecast(data);
       });
   });
@@ -98,10 +136,6 @@ const getFiveDayForecast = function(city){
 const displayFiveDayForecast = function(weather) {
   let fiveDayForecastContainerEl = document.querySelector("#five-day-forecast-container");
     fiveDayForecastContainerEl.textContent = '';
-  
-  // let fiveDayForecastHeaderEl = document.createElement("div");
-  // fiveDayForecastContainerEl.appendChild(fiveDayForecastHeaderEl);
-  
   
     fiveDayForecastHeader.classList = "col-12 h2";
     fiveDayForecastHeader.textContent = "5-Day Forecast";
@@ -112,9 +146,9 @@ const displayFiveDayForecast = function(weather) {
     fiveDayForecastContainerEl.appendChild(fiveDayForecastCardGroup);
 
   let fiveDayForecast = weather.list;
-    for (let i=5; i<fiveDayForecast.length; i+=8) {
+    for (let i=7; i<fiveDayForecast.length; i+=8) {
       let daily = fiveDayForecast[i];
-      console.log(daily); 
+      //console.log(daily); 
 
       let fiveDayForecastCards = document.querySelector("#five-day-forecast-cards");
       fiveDayForecastCards = document.createElement("div");
@@ -123,8 +157,7 @@ const displayFiveDayForecast = function(weather) {
       
       let fiveDayForecastDate = document.createElement("h4");
       fiveDayForecastDate.classList = "card-header";
-      fiveDayForecastDate.textContent = moment.unix(daily.dt).format("MMM D, YYYY");
-
+      fiveDayForecastDate.textContent = moment.unix(daily.dt).format("MMM D, YYYY HH:MM:SS");
       fiveDayForecastCards.appendChild(fiveDayForecastDate);
 
       let fiveDayForecastIcon = document.createElement("img");
@@ -143,9 +176,5 @@ const displayFiveDayForecast = function(weather) {
       fiveDayForecastHumidity.textContent = daily.main.humidity + "  %";
       fiveDayForecastCards.appendChild(fiveDayForecastHumidity);
 
-      let fiveDayForecastUvIndex = document.createElement("span");
-      fiveDayForecastUvIndex.classList = "card-body";
-      fiveDayForecastUvIndex.textContent = 'UV Index here';
-      fiveDayForecastCards.appendChild(fiveDayForecastUvIndex);
     }
-};
+  
